84b17cee2f45105f51c5c7b1e1776c0c8cbe4f47
s0l0n3t <furkantokgz@gmail.com>
2024-06-18 13:06:05 +0300

    China Zodiac calculation problem.

--- lecture_3/lecture-notes3/src/App.java
+++ lecture_3/lecture-notes3/src/App.java
@@ -141,7 +141,7 @@ public class App {
         }
         else{
             System.out.println("There is equalisation !");
-        }*/
+        }*//*
         int pathKm,age,type;
         double cash;
 
@@ -177,8 +177,56 @@ public class App {
             System.out.println("Hatalı Veri Girdiniz !");
         }
 
-        System.out.println(cash);
+        System.out.println(cash);*/
 
+
+
+
+
+        int month=0;
+        System.out.print("Ay> ");
+        month = coScanner.nextInt();
+        switch(month%12){
+
+            case 0:
+            System.out.println("Maymun");
+            case 1:
+            System.out.println("Horoz");
+            break;
+            case 2:
+            System.out.println("Köpek");
+            break;
+            case 3:
+            System.out.println("Domuz");
+            break;
+            case 4:
+            System.out.println("Fare");
+            break;
+            case 5:
+            System.out.println("Okuz");
+            break;
+            case 6:
+            System.out.println("Kaplan");
+            break;
+            case 7:
+            System.out.println("Tavsan");
+            break;
+            case 8:
+            System.out.println("Ejderha");
+            break;
+            case 9:
+            System.out.println("Yılan");
+            break;
+            case 10:
+            System.out.println("At");
+            break;
+            case 11:
+            System.out.println("Koyun");
+            break;
+            default:
+                break;
+
+        }
 
     }
 }
