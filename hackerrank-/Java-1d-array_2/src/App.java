25f4f28b5bb1251a2fcee377254cbdb2e14928ec
s0l0n3t <furkantokgz@gmail.com>
2024-07-01 12:28:28 +0300

    hackerrank medium difficult array challange done.

--- hackerrank-/Java-1d-array_2/src/App.java
+++ hackerrank-/Java-1d-array_2/src/App.java
@@ -3,148 +3,52 @@ import java.text.BreakIterator;
 import java.util.Arrays;
 
 import javax.lang.model.util.ElementScanner14;
+import javax.management.Query;
+import javax.sound.midi.Soundbank;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner arrayScanner = new Scanner(System.in);
-        int queries = arrayScanner.nextInt();
-        
-         // number - queries
-        //queries şuanda number olarak kullanılıyor.
-        
-        
-        
-        /*
-        for(int k = 0;k<numberTest1.length;k++){
-            numberTest[0][k] = numberTest1[k];
-        }
-        for(int a = 0;a<numberTest.length;a++){
-            for(int j = 0;j< numberTest[a].length;j++){
-                System.out.print(numberTest[a][j]+ " ");
-            }
-            System.out.println();
-        }
-         */
+            Scanner arrayScanner = new Scanner(System.in);
+            int qeuries = arrayScanner.nextInt();
+            while(qeuries-- > 0){
+                int numberArray = arrayScanner.nextInt();
+                int leap = arrayScanner.nextInt();
 
-        
+                int[] arrayGame = new int[numberArray];
 
-
-
-
-        if(queries >= 1 && queries <= 5000){
-            int[] arrayN = new int[queries];
-            int[] arrayL = new int[queries];
-            
-            String[] numberStringTest = new String[queries];
-
-            for(int i = 0;i< queries;i++){
-                int index = 0;
-                arrayN[i] = arrayScanner.nextInt();
-                arrayL[i] = arrayScanner.nextInt();
-                int[] numberTest1= new int[arrayN[i]];
-                for(int a = 0;a< arrayN[i];a++){
-                    
-                    numberTest1[a] = arrayScanner.nextInt();
-                }/*
-                System.out.println("OUTPUT: ");
-        
-                System.out.println(Arrays.toString(numberTest1));
- */
-                
-                if(numberTest1[index+1] == 0){
-                    index += 1;//Walk to 1
-                    try{
-                        if(isTrue(index, numberTest1, arrayL, i)){
-                            numberStringTest[i] = "YES";
-                            
-                        }
-                        else{
-                            numberStringTest[i] = "NO";
-                        }
-                    }
-                    catch(Exception e){
-                        numberStringTest[i] = "YES";
-                    }
-                    //Buradan devam ediliyor. Oyun algoritması yazılıp sonucu string dizisine yazılacak.
-
+                for(int i = 0;i<numberArray;i++){
+                    arrayGame[i] = arrayScanner.nextInt();
+                }
+                if(isPlay(leap, arrayGame)){
+                    System.out.println("YES");
                 }
                 else{
-                    index -= 1;
-
-                    if(isTrue(index, numberTest1, arrayL, i)){
-                        numberStringTest[i] = "YES";
-                        
-                    }
-                    else{
-                        numberStringTest[i] = "NO";
-                        
-                    }
-                    // string array e yes gidecek.
-                    //numberStringTest[i] = "YES";
+                    System.out.println("NO");
                 }
-
             }
-            for(int a = 0;a<numberStringTest.length;a++){
-                System.out.println(numberStringTest[a]);
-            }
-
-            
-            /*
-            for(int i = 0;i<queries;i++){
-                if(arrayN[i] >= 2 && arrayL[i] <= 100){
-                    
-                    if(arrayL[i] >= 0 && arrayL[i] <= 100){
-                        
-                        if(arrayN[i] == 5 && arrayL[i] == 3){
-                            System.out.println("YES");
-                            
-                        }
-                        else if(arrayN[i]== 6 && arrayL[i] == 5){
-                            System.out.println("YES");
-                            
-                        }
-                        else if(arrayN[i] == 6 && arrayL[i] == 3){
-                            System.out.println("NO");
-                           
-                        }
-                        else{
-                            System.out.println("NO");
-                                                   }
-                        // 0 0 0 0 0
-                        
-                    }
-                    else{
-                        System.out.println("HATA");
-                        
-                    }
-           
-                }
-            } */
-               
-            
+            arrayScanner.close();
         }
- 
-    }
+        static boolean isPlay(int l,int[] gameInput){
+           
+            return isOver( 0, l, gameInput);
+        }
 
-    static boolean isTrue(int index,int[] numberTest1,int[] arrayL,int i){
-            
-            if(index + arrayL[i] > numberTest1.length){
-                // string array e yes gidecek. //OYUN BİTTİ
-                //numberStringTest[i] = "YES";
-                return true;
-                
-            }
-            index += arrayL[i];
-            if(numberTest1[index] == 0){
 
-                return isTrue(index, numberTest1, arrayL, i);
-            }
-            else{
+        static boolean isOver(int i, int leap, int[] gameInput){
+            if(i < 0 || gameInput[i] == 1){
                 return false;
             }
+            else if(i >= gameInput.length){
+                return true;
+            }
+            gameInput[i] = 1;
+            return isOver(i+leap, leap, gameInput) || isOver(i+1, leap, gameInput) || isOver(i-1, leap, gameInput);
+        }
     }
+
+
+
 
 
 
     
-}
