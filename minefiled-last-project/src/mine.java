2e76234e0fc04f4115eaf49e6026cac1f5f7da87
s0l0n3t <furkantokgz@gmail.com>
2024-07-02 20:26:05 +0300

    minefield project creating mine field

--- minefiled-last-project/src/mine.java
+++ minefiled-last-project/src/mine.java
@@ -4,28 +4,56 @@ public class mine {
 
     public static void createMinefield(int high, int width){
         String[][] mineMatris = new String[high][width];
-        int mineCount = high*width;
+        int mineCount = (high*width)/4;
+        int mineHeigh= 0;
+        int mineWidth= 0;
+
+        for(int j = 0;j< mineMatris.length;j++){ //Creating mine field
+            for(int k = 0; k< mineMatris[j].length;k++){
+                mineMatris[j][k] = "-";
+            }
+        }
+       
         for(int i = 0;i<mineCount;i++){
-            int mineCoord = (int)Math.random() * ((high*width) -1);
+           
+            int mineCoord = (int)Math.random()*10;
+            System.out.println(mineCoord);// Warning. I cant get random number.
             // 4
             /*  - - - 03
              *  - - - 10
              *  - - -
              */
-            findCoord(width, mineCoord); //Find coord for mines.
+            mineWidth = findCoordWidth(width, mineCoord); //Find width
+            while(mineCoord > width){
+                mineCoord -= width; // 5
+                
+                mineHeigh++; //Heigh
+            }
+
+            mineMatris[mineHeigh][mineWidth] = "*";
+
+            
         }
+        
+        for(int j = 0;j< mineMatris.length;j++){ //Creating mine field
+            for(int k = 0; k< mineMatris[j].length;k++){
+                System.out.print(mineMatris[j][k] + " ");
+            }
+            System.out.println();
+        }
+
+
 
     }
 
-    public static int findCoord(int width,int mineCoord){
-        int count = 0;
+    
+    public static int findCoordWidth(int width,int mineCoord){
+        
         if(mineCoord > width){
-            mineCoord -= width; // 5
-            count++;
-            return count; //gonna be high for matris
+            mineCoord -= width; 
+            return findCoordWidth(width, mineCoord);
         }
-       
-        return findCoord(width, mineCoord);
+        return mineCoord; // width
     }
 
 }
