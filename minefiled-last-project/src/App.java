44ed030dfcc065aa6c8ae882867471e6b93376e2
s0l0n3t <furkantokgz@gmail.com>
2024-07-04 16:36:13 +0300

    mine field game is done.

--- minefiled-last-project/src/App.java
+++ minefiled-last-project/src/App.java
@@ -4,18 +4,71 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Mayın tarlasına Hoşgeldiniz");
+        System.out.println("Welcome to mine field !");
         
         Scanner matScanner = new Scanner(System.in);
-        int matrisHigh = matScanner.nextInt();
-        int matrisWidth = matScanner.nextInt();
-        if(matrisHigh >= 2 && matrisWidth >= 2){
-            mine.createMinefield(matrisHigh, matrisWidth); //Mine field creating error. 
+        int matrisRow = matScanner.nextInt();
+        int matrisColumn = matScanner.nextInt();
+        int mineCount = (matrisRow*matrisColumn)/4;
+        int[][] mineCoordArray = new int[mineCount][2];
+        int mineFoundRow = 0;
+        int mineFoundColumn = 0;
+        String[][] mineFoundStrings = new String[matrisRow][matrisColumn];
 
-        } 
+        for(int k = 0;k<mineFoundStrings.length;k++){//Creating finding matris
+            for(int j = 0; j<mineFoundStrings[0].length;j++){
+                mineFoundStrings[k][j] = "-";
+            }
+            System.out.println();
+        }
+        int mineFound = 0;
+        System.out.println();
+        if(matrisRow >= 2 && matrisColumn >= 2){
+            mine.createMinefield(matrisRow, matrisColumn,mineCount,mineCoordArray); //Mine field function
+            while(mineFound == 0){
+            System.out.print("Type Row : ");
+            int guessRow = matScanner.nextInt();
+            System.out.print("Type Column : ");
+            int guessColumn = matScanner.nextInt();System.out.println("==========================");
+            
+            for(int i = 0;i<mineCoordArray.length;i++){//Searching
+                if(mineCoordArray[i][0] == guessRow && mineCoordArray[i][1] == guessColumn){
+                    mineFound++;
+                    
+                }
+            }
+            if(mineFound > 0 ){
+            //Creating finding matris
+            mineFoundStrings[guessRow][guessColumn] = "0";
+            for(int k = 0;k<mineFoundStrings.length;k++){//Creating finding matris
+                for(int j = 0; j<mineFoundStrings[0].length;j++){
+                    System.out.print(mineFoundStrings[k][j]+ " ");
+                }
+                System.out.println();
+            }
+            System.out.println("Game Over !!");
+            break;
+            }
+            else{
+                mineFoundStrings[guessRow][guessColumn] = Integer.toString(mineCount);
+            }
+            
+            for(int k = 0;k<mineFoundStrings.length;k++){//Creating finding matris
+                for(int j = 0; j<mineFoundStrings[0].length;j++){
+                    System.out.print(mineFoundStrings[k][j]+ " ");
+                }
+                System.out.println();
+            }
 
-
-
+            
+        }
+        
+            
+        }
+        else{
+            System.out.println("Row or column numbers are not enough !");
+        }
+        matScanner.close();
 	}
 
     }
--- minefiled-last-project/src/mine.java
+++ minefiled-last-project/src/mine.java
@@ -1,13 +1,15 @@
 public class mine {
+    int row = 0;
+    int column = 0;
+    
 
-
-
-    public static void createMinefield(int high, int width){
-        String[][] mineMatris = new String[high][width];
-        int mineCount = (high*width)/4;
-        int mineHeigh= 0;
-        int mineWidth= 0;
-
+    public static void createMinefield(int row, int column, int mineCount,int[][] mineCoordArray){
+        mine accMine = new mine();
+        String[][] mineMatris = new String[row][column];
+        accMine.row = row;
+        accMine.column = column;
+        int mineRow= 0;
+        int mineColumn= 0;
         for(int j = 0;j< mineMatris.length;j++){ //Creating mine field
             for(int k = 0; k< mineMatris[j].length;k++){
                 mineMatris[j][k] = "-";
@@ -16,44 +18,34 @@ public class mine {
        
         for(int i = 0;i<mineCount;i++){
            
-            int mineCoord = (int)Math.random()*10;
-            System.out.println(mineCoord);// Warning. I cant get random number.
+            int mineCoord = (int)(Math.random()*((column*row)-1));
+            
             // 4
             /*  - - - 03
              *  - - - 10
              *  - - -
              */
-            mineWidth = findCoordWidth(width, mineCoord); //Find width
-            while(mineCoord > width){
-                mineCoord -= width; // 5
-                
-                mineHeigh++; //Heigh
-            }
-
-            mineMatris[mineHeigh][mineWidth] = "*";
-
-            
+            mineColumn = findCoordColumn(column, mineCoord); //Find width
+            mineRow = mineCoord / row;
+            mineMatris[mineRow][mineColumn] = "*";
+            mineCoordArray[i][0] = mineRow;
+            mineCoordArray[i][1] = mineColumn;
         }
         
-        for(int j = 0;j< mineMatris.length;j++){ //Creating mine field
+        for(int j = 0;j< mineMatris.length;j++){ //Print mine field
             for(int k = 0; k< mineMatris[j].length;k++){
                 System.out.print(mineMatris[j][k] + " ");
             }
             System.out.println();
         }
-
-
-
     }
 
     
-    public static int findCoordWidth(int width,int mineCoord){
-        
-        if(mineCoord > width){
-            mineCoord -= width; 
-            return findCoordWidth(width, mineCoord);
+    public static int findCoordColumn(int column,int mineCoord){
+        if(mineCoord >= column){
+            mineCoord -= column; 
+            return findCoordColumn(column, mineCoord);
         }
         return mineCoord; // width
     }
-
 }
