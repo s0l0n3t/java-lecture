5c84b7574742c23351a279efcc599341c11de244
s0l0n3t <furkantokgz@gmail.com>
2025-03-29 16:30:42 +0300

    lecture complated

--- lecture_java_spring/OtherSpringBoot/src/main/java/com/furkantokgoz/config/WebSecurityConfig.java
+++ lecture_java_spring/OtherSpringBoot/src/main/java/com/furkantokgoz/config/WebSecurityConfig.java
@@ -1,13 +1,9 @@
-package com.furkantokgoz.security.user.servicesimpl;
+package com.furkantokgoz.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
